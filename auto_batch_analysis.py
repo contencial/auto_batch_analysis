5c3e5148de576f116e6d628ca65b5b3368764a51
kefujiwa <kefujiwa@student.42tokyo.jp>
2022-11-21 17:23:06 +0900

    fix: 要素を特定できないバグを修正

--- auto_batch_analysis.py
+++ auto_batch_analysis.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
 from fake_useragent import UserAgent
 from oauth2client.service_account import ServiceAccountCredentials
 from webdriver_manager.chrome import ChromeDriverManager
@@ -61,58 +62,78 @@ def batch_analysis(domain_info):
 
         driver.get(url_login)
         driver.maximize_window()
+        driver.implicitly_wait(60)
 
-        driver.find_element_by_name('email').send_keys(login)
-        driver.find_element_by_name('password').send_keys(password)
-        driver.find_element_by_xpath('//button[@type="submit"]').click()
+        driver.find_element(By.NAME, 'email').send_keys(login)
+        driver.find_element(By.NAME, 'password').send_keys(password)
+        driver.find_element(By.XPATH, '//input[@type="checkbox"]').click()
+        sleep(1)
+        driver.find_element(By.XPATH, '//button[@type="submit"]').click()
         logger.info(f'batch_analysis: Sign in')
         driver.implicitly_wait(60)
 
-        driver.find_element_by_xpath('//button[@data-toggle="dropdown"]').click()
-        driver.implicitly_wait(20)
-        driver.find_element_by_xpath('//a[@href="/batch-analysis"]').click()
+        driver.find_element(By.XPATH, '//a[@href="/batch-analysis"]')
+        driver.get(url_ba)
         sleep(5)
 
         domain_list = list(split_list(domain_info, 200))
-        dr_list = list()
-        ip_list = list()
+        logger.debug(f'domain_list: size: {len(domain_list)}')
+        drl = []
+        ipl = []
+        gov = []
+        edu = []
         for domain_chunk in domain_list:
-            search = driver.find_element_by_id('batch_requests')
-            search.send_keys("\n".join(domain_chunk))
-            driver.find_element_by_id('startAnalysisButton').click()
             driver.implicitly_wait(60)
-            dr = driver.find_elements_by_name('domain_rating')
-            dr_list.extend(list(extract_text(dr)))
-            ip = driver.find_elements_by_xpath('//tr/td[15]')
-            ip_list.extend(list(extract_text(ip)))
+            search = driver.find_element(By.ID, 'batch_requests')
+            search.send_keys("\n".join(domain_chunk))
+            driver.find_element(By.ID, 'startAnalysisButton').click()
+            driver.implicitly_wait(600)
+            driver.find_element(By.ID, 'batch_data_table')
+            sleep(3)
+
+            drl_dr = driver.find_elements(By.NAME, 'domain_rating')
+            sleep(1)
+            ipl_dr = driver.find_elements(By.XPATH, '//*[@id="batch_data_table"]/tbody/tr/td[15]')
+            sleep(1)
+            gov_dr = driver.find_elements(By.XPATH, '//*[@id="batch_data_table"]/tbody/tr/td[12]')
+            sleep(1)
+            edu_dr = driver.find_elements(By.XPATH, '//*[@id="batch_data_table"]/tbody/tr/td[13]')
+            sleep(1)
+
+            drl.extend(list(extract_text(drl_dr)))
+            ipl.extend(list(extract_text(ipl_dr)))
+            gov.extend(list(extract_text(gov_dr)))
+            edu.extend(list(extract_text(edu_dr)))
+            sleep(2)
             driver.get(url_ba)
+
+        logger.debug(f'Size: dnl: {len(domain_info)}')
+        logger.debug(f'Size: drl: {len(drl)}')
+        logger.debug(f'Size: ipl: {len(ipl)}')
+        logger.debug(f'Size: gov: {len(gov)}')
+        logger.debug(f'Size: edu: {len(edu)}')
+        result = []
+        for dn, dr, ip, go, ed in zip(domain_info, drl, ipl, gov, edu):
+            result.append([dn, dr, ip, go, ed])
 
         driver.close()
         driver.quit()
 
-        return [dr_list, ip_list]
+        return result
     except Exception as err:
         logger.error(f'batch_analysis: {err}')
 
-def write_batch_info(sheet, dr, ip):
+def write_batch_info(sheet, data):
     SPREADSHEET_ID = os.environ['DOMAIN_LIST_SSID']
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name('spreadsheet.json', scope)
     gc = gspread.authorize(credentials)
     sheet = gc.open_by_key(SPREADSHEET_ID).worksheet(sheet)
 
-    cell_list = sheet.range('C2:C' + str(len(dr) + 1))
-    i = 0
-    for cell in cell_list:
-        cell.value = dr[i]
-        i += 1
-    sheet.update_cells(cell_list, value_input_option='USER_ENTERED')
+    cell_list = sheet.range('B2:F' + str(len(data) + 1))
+    for index, cell in enumerate(cell_list):
+        cell.value = data[int(index / 5)][int(index % 5)]
 
-    cell_list = sheet.range('D2:D' + str(len(ip) + 1))
-    i = 0
-    for cell in cell_list:
-        cell.value = ip[i]
-        i += 1
     sheet.update_cells(cell_list, value_input_option='USER_ENTERED')
 
 ### main_script ###
@@ -120,10 +141,10 @@ if __name__ == '__main__':
     try:
         domain_info = get_domain_info('List-Japanese')
         batch_info = batch_analysis(domain_info)
-        write_batch_info('List-Japanese', batch_info[0], batch_info[1])
+        write_batch_info('List-Japanese', batch_info)
         domain_info = get_domain_info('List-NotJapanese')
         batch_info = batch_analysis(domain_info)
-        write_batch_info('List-NotJapanese', batch_info[0], batch_info[1])
+        write_batch_info('List-NotJapanese', batch_info)
         logger.info('Finish')
         exit(0)
     except Exception as err:
